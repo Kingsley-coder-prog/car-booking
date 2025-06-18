3bb585f1c16ad2bc98d7b37d8ae0484b25ad65d1
Kingsley <kifeanyi216@gmail.com>
2025-06-18 15:05:50 +0100

    Menu-bar

--- src/components/layout/PageHeader.vue
+++ src/components/layout/PageHeader.vue
@@ -1,8 +1,11 @@
 <template>
-  <header class="flex justify-centre px-6 z-50 fixed w-full top-0 p-2">
+  <header
+    :class="headerClass"
+    class="flex justify-centre px-6 z-50 fixed w-full top-0 p-2 bg-white drop-shadow-sm"
+  >
     <div class="flex justify-between items-centre w-full md:w-5/6 md:relative">
       <PageLogo />
-      <nav class="space-x-6 p-2">
+      <nav class="space-x-6 p-2 hidden md:flex">
         <router-link
           v-for="link in links"
           :key="link.path"
@@ -11,6 +14,31 @@
           >{{ link.name }}</router-link
         >
       </nav>
+
+      <!-- Mobile Hamburger Menu -->
+      <div class="flex md:hidden">
+        <button
+          @click="toggleMenu"
+          class="text-gray-800 focus:outline-none"
+          aria-label="Toggle mobile menu"
+        >
+          <FaBarsStaggered />
+        </button>
+
+        <div
+          v-if="menuVisible"
+          class="absolute top-16 left-0 bg-white shadow-lg w-full p-4 flex flex-col space-y-6"
+        >
+          <router-link
+            v-for="link in links"
+            :key="link.path"
+            :to="link.path"
+            :class="{ 'active-link': route.path === link.path }"
+            @click="closeMenu"
+            >{{ link.name }}</router-link
+          >
+        </div>
+      </div>
     </div>
   </header>
 </template>
@@ -18,10 +46,20 @@
 <script setup>
 import PageLogo from "../composables/PageLogo.vue";
 import { useRoute } from "vue-router";
+import { computed, ref, onBeforeUnmount, onMounted } from "vue";
+import { FaBarsStaggered } from "@kalimahapps/vue-icons";
 
 const route = useRoute();
 const isScrolled = ref(false);
 const menuVisible = ref(false);
+
+const toggleMenu = () => {
+  menuVisible.value = !menuVisible.value;
+};
+
+const closeMenu = () => {
+  menuVisible.value = false;
+};
 
 const links = [
   { name: "Home", path: "/" },
@@ -36,7 +74,22 @@ const links = [
 const headerClass = computed(() => ({
   "sm:bg-transparent": !isScrolled.value,
   "sm:bg-white": isScrolled.value,
+  "shadow-md": isScrolled.value,
+  "p-4": isScrolled.value,
+  "sm:py-10": !isScrolled.value,
 }));
+
+const handleScroll = () => {
+  isScrolled.value = window.scrollY > 50;
+};
+
+onMounted(() => {
+  window.addEventListener("scroll", handleScroll);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener("scroll", handleScroll);
+});
 </script>
 
 <style>
